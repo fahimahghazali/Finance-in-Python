cba1c64316996b220baa0922079ad5eaf7dca4e3
Nurfahimah Mohd Ghazali <88434296+fahimahghazali@users.noreply.github.com>
2022-05-04 10:30:00 +0100

    Add files via upload

---
+++ stocktrader.py
@@ -0,0 +1,33 @@
+"""
+stocktrader -- A Python module for virtual stock trading
+TODO: Add a description of the module...
+Also fill out the personal fields below.
+
+Full name: Peter Pan
+StudentId: 123456
+Email: peter.pan.123@student.manchester.ac.uk
+"""
+
+class TransactionError(Exception):
+    pass
+
+class DateError(Exception):
+    pass
+
+stocks = {}
+portfolio = {}
+transactions = []
+
+def normaliseDate(s): 
+    # TODO
+    return
+
+# TODO: All other functions from the tasks go here
+
+def main():
+    # Test your functions here
+    return
+
+# the following allows your module to be run as a program
+if __name__ == '__main__' or __name__ == 'builtins':
+    main()
